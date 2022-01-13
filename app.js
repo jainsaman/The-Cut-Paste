e3b2d78650143b38e2b732f6771c820cf4a79861
Akarsh <mailakarshshrivastava@gmail.com>
2022-01-13 23:20:50 +0530

    update

--- app.js
+++ app.js
@@ -51,15 +51,12 @@ let newspapers = [
         url: "./newspaper.pdf",
         date: "July 2023",
     },
-
-
-
 ]
 // Mapping All Newspapers
 
 document.getElementById('card-container').innerHTML = newspapers.map(newspaper =>
     `<div class="card m-3 w-75 mt-3">
-        <div class="card-body w-100 d-flex justify-content-between">
+        <div class="card-body w-100 d-flex justify-content-between align-items-center">
             <h5 class="card-title">${newspaper.date}</h5>
             <button id="read-btn" type="button" class="btn btn-outline-danger">
             <a href=${newspaper.url} target="_blank">Read Now</a>
