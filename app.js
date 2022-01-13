b9b18d56d2ed72c1ca27e73a91438c22cfdcc7df
Akarsh <mailakarshshrivastava@gmail.com>
2022-01-13 23:24:52 +0530

    edit styles

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ let newspapers = [
 document.getElementById('card-container').innerHTML = newspapers.map(newspaper =>
     `<div class="card m-3 w-75 mt-3">
         <div class="card-body w-100 d-flex justify-content-between align-items-center">
-            <h5 class="card-title">${newspaper.date}</h5>
+            <h5 class="m-0 card-title">${newspaper.date}</h5>
             <button id="read-btn" type="button" class="btn btn-outline-danger">
             <a href=${newspaper.url} target="_blank">Read Now</a>
           </button>
