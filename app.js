85ad9baf903962c5503e9d07320411cdb8543cd7
Akarsh <mailakarshshrivastava@gmail.com>
2022-01-13 18:44:56 +0530

    try2

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ document.getElementById("allNewspapers-btn").addEventListener("click", (event) =
     }
 });
 
+// All Newspapers Functionality
+
 // Sample Newspapers Data
 let newspapers = [
     {
@@ -71,21 +73,16 @@ let newspapers = [
         date: "2022",
     },
 ]
-console.log(newspapers)
-newspapers.map(newspaper => {
-    console.log(
-        newspaper.date,
-        newspaper.url
-    )
-})
 
-// All Newspapers Functionality
+
+// Mapping All Newspapers
+
 document.getElementById('card-container').innerHTML = newspapers.map(newspaper =>
     `<div class="card m-3 w-25 mt-3 d-inline-flex">
         <div class="card-body">
             <h5 class="card-title">${newspaper.date}</h5>
             <button type="button" class="btn-outline-danger">
-            <a href="#" target="_blank">Read Now</a>
+            <a href=newspaper.url target="_blank">Read Now</a>
           </button>
         </div>
     </div>
